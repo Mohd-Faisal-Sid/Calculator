73bffdedb0ebcb8b4dc2acbc4fe9e537f0990d14
Mohd-Faisal-Sid <mfs959839@gmail.com>
2023-10-25 10:19:15 +0530

    Calculator Add Some File

---
+++ Calculetor.js
@@ -0,0 +1,87 @@
+/*let age= 19;
+let voter_card= "No";
+if(age == 18 && voter_card == "No"){
+    alert("You Are Elegible for one time Vote but You can Apply for Voter_Card");
+}else if(age > 18 && voter_card == "Yes"){
+        alert("You Are Elegible for Vote !!");
+} else if(age > 18 && voter_card == "No"){
+    alert("Please Apply for Voter Id Card .");
+}else{
+    alert("You are Not Elegible for vote");
+}
+
+let log_in = 2; // 0 log_out or 1 log_in
+
+if(log_in == 0){
+    document.write('log_in');
+}else{
+    document.write('log_out');
+}
+
+let log_in =2;
+let option = log_in ==1 ? "log_out" : "log_in ";
+    document.write(option);
+
+let Sign_in = 1;
+
+let option = Sign_in == 1? "Sign_up": "Sign_in";
+    document.write(option);
+
+let User;
+
+// User = "Faisal";
+
+alert(User ?? "Guest User");
+let input = "0";
+if(input == 1){
+    alert("Continue..");
+}else if( input == "y"){
+    alert("Continue...");
+} else if(input == "yes"){
+    alert("Continue");
+}else if(input == 0){
+    alert("End..");
+}else if(input =="n"){
+    alert("End..");
+}else if(input == "no"){
+    alert("End..");
+}else{
+    alert("Wrong input");
+}
+let input = "no";
+switch(input){
+    case 1:
+        document.write("continue..");
+        break;
+    case "y" :
+        document.write("continue..");
+        break;
+    case "yes" :
+        document.write("continue..");
+        break;
+    case "0" :
+        document.write("End..");
+        break;
+    case "n" :
+        document.write("End..");
+        break;
+    case "no" :
+        document.write("End..");
+        break;
+    default :
+    document.write("Wrong Input");
+    // break; lga bhi sakte h nhii bhi
+}
+let input = ;
+if(input == 1|| input=="y"|| input == "yes"){
+    document.write("Continue..");
+}else if(input == 0|| input=="n"|| input == "no"){
+    document.write("End..");
+}else{
+    document.write("Wrong input");
+}*/
+let counter = 1;
+ while(counter <= 10){
+    document.write("TechGun");
+    
+}
