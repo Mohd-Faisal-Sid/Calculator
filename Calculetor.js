4feefd96bb0d6176cb771cbde507a314c4d11ae1
Mohd-Faisal-Sid <mfs959839@gmail.com>
2023-10-27 15:51:48 +0530

    Calculator all Function are done in Calculatotr.js

--- Calculetor.js
+++ Calculetor.js
@@ -1,87 +1,107 @@
-/*let age= 19;
-let voter_card= "No";
-if(age == 18 && voter_card == "No"){
-    alert("You Are Elegible for one time Vote but You can Apply for Voter_Card");
-}else if(age > 18 && voter_card == "Yes"){
-        alert("You Are Elegible for Vote !!");
-} else if(age > 18 && voter_card == "No"){
-    alert("Please Apply for Voter Id Card .");
-}else{
-    alert("You are Not Elegible for vote");
-}
+const resultElement = document.getElementById('result');
+const clrbtn = document.getElementById('clear-button');
+const delbtn = document.getElementById('delete-button');
+const divbtn = document.getElementById('divide-button');
+const mulbtn = document.getElementById('multiplication-button');
+const subbtn = document.getElementById('substraction-button');
+const addbtn = document.getElementById('addition-button');
+const decbtn = document.getElementById('decimal-button');
+const eqlbtn = document.getElementById('equal-button');
+const numbtns = document.querySelectorAll('.number');
 
-let log_in = 2; // 0 log_out or 1 log_in
+// Intialized the Variable
+let result ="";
+let operation="";
+let previousoprand = 0;
 
-if(log_in == 0){
-    document.write('log_in');
-}else{
-    document.write('log_out');
-}
-
-let log_in =2;
-let option = log_in ==1 ? "log_out" : "log_in ";
-    document.write(option);
-
-let Sign_in = 1;
-
-let option = Sign_in == 1? "Sign_up": "Sign_in";
-    document.write(option);
-
-let User;
-
-// User = "Faisal";
-
-alert(User ?? "Guest User");
-let input = "0";
-if(input == 1){
-    alert("Continue..");
-}else if( input == "y"){
-    alert("Continue...");
-} else if(input == "yes"){
-    alert("Continue");
-}else if(input == 0){
-    alert("End..");
-}else if(input =="n"){
-    alert("End..");
-}else if(input == "no"){
-    alert("End..");
-}else{
-    alert("Wrong input");
-}
-let input = "no";
-switch(input){
-    case 1:
-        document.write("continue..");
-        break;
-    case "y" :
-        document.write("continue..");
-        break;
-    case "yes" :
-        document.write("continue..");
-        break;
-    case "0" :
-        document.write("End..");
-        break;
-    case "n" :
-        document.write("End..");
-        break;
-    case "no" :
-        document.write("End..");
-        break;
-    default :
-    document.write("Wrong Input");
-    // break; lga bhi sakte h nhii bhi
-}
-let input = ;
-if(input == 1|| input=="y"|| input == "yes"){
-    document.write("Continue..");
-}else if(input == 0|| input=="n"|| input == "no"){
-    document.write("End..");
-}else{
-    document.write("Wrong input");
-}*/
-let counter = 1;
- while(counter <= 10){
-    document.write("TechGun");
+// function to select oprator
+const selectOperator =(operatorValue) =>{
+    if(result === '')  return;
     
+    if (operation !== '' && previousoprand !== ''){
+        calculateResult();
+    }
+    operation = operatorValue
+    previousoprand = result;
+    result= '';
+    updateDisplay();
 }
+// function to calculate Result
+const calculateResult = () => {
+    let evaluateResult;
+
+    const prev = parseFloat(previousoprand) ;
+    const current =parseFloat(result);
+
+    if(isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+' :
+            evaluateResult = prev + current;
+            break;
+        case '-' :
+             evaluateResult = prev - current;
+            break;
+        case '*' :
+            evaluateResult = prev * current;
+            break;
+         case '/' :
+            evaluateResult = prev / current;
+            break;
+
+        default:
+            return;
+    }
+    result = evaluateResult.toString();
+    operation = '';
+    previousoprand ='';
+}
+//funtion to append number
+const appendnumber = (number) =>{
+    if(number === '.' && result.includes('.')) return;
+    result += number;
+    updateDisplay();
+}
+// function to update display
+const updateDisplay = () => {
+    if(operation){
+        resultElement.innerText = `${previousoprand} ${operation} ${result}`;
+    }
+    else{
+        resultElement.innerText = result;
+    }
+}
+// Add event listener to number buttons
+numbtns.forEach(button =>{
+    button.addEventListener('click', ()=> {
+        appendnumber(button.innerText);
+    })
+})
+// function to clear display
+const clearDisplay =() =>{
+    result= '';
+    previousoprand= '';
+    operation = '';
+    updateDisplay();
+}
+// function to delete Last button
+const deleteLastDigit =() => {
+    if(result === '') return;
+    result = result.slice(0 , -1);
+    updateDisplay();
+}
+
+
+
+decbtn.addEventListener('click' , () => appendnumber('.'));
+addbtn.addEventListener('click', () => selectOperator('+'));
+subbtn.addEventListener('click', () => selectOperator('-'));
+mulbtn.addEventListener('click', () => selectOperator('*'));
+divbtn.addEventListener('click', () => selectOperator('/'));
+eqlbtn.addEventListener('click', () => {
+    if(result ==='') return;
+    calculateResult();
+    updateDisplay();
+});
+clrbtn.addEventListener('click', clearDisplay);
+delbtn.addEventListener('click', deleteLastDigit);
